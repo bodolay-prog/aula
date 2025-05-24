695e714f6ef4110db5b6035433d336d7464707da
bodolay-prog <caua.bodolay09@aluno.ifce.edu.br>
2025-05-24 21:30:46 +0000

    Final

--- desafio.c
+++ desafio.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define range 8 
+#define rangebits 8 
 int main()
 {
     unsigned char sys= 0x00;
@@ -7,7 +7,7 @@ int main()
     unsigned  to;
     
 // Preenchendo os status
-for(int i=0;i<range-4;i++)
+for(int i=0;i<rangebits-4;i++)
     {
         printf("Digite o status da máquina %d: ", i+1);
         scanf("%d",&to);
@@ -16,37 +16,37 @@ for(int i=0;i<range-4;i++)
             sys = sys|(mask<<i);    
         }
 
-        if(to!=0||to!=1)
-        {
-            puts("Digite um status correto.");
-            i--;
-            continue;
-        }
+        if(to!=1&&to!=0)
+    {
+        puts("Digite um status correto.");
+        i--;
+        continue;
+    }
 
 
         printf("Digite o status de leitura da máquina %d: ", i+1);
         scanf("%d",&to);
         if(to==1)
         {
-            sys = sys|(mask<<(i+4));    
+            sys = sys|(mask<<(i+(rangebits-4)));    
         }
 
-        if(to!=0||to!=1)
-        {
-            puts("Digite um status correto.");
-            i--;
-            continue;
-        }
+        if(to!=1&&to!=0)
+    {
+        puts("Digite um status correto.");
+        i--;
+        continue;
+    }
     
     }
     
 
 //Mostrando o status do sistema completo
-for(int i=0;i<range-4;i++)
+for(int i=0;i<rangebits-4;i++)
 {
     
     (sys & (mask << (i)))?printf("Equipamento %d - Ligado - ",i+1):printf("Equipamento %d - Desligado - ",i+1);
-    (sys & (mask << ((range-4)+i)))?printf("Alto\n"):printf("Baixo\n");
+    (sys & (mask << ((rangebits-4)+i)))?printf("Alto\n"):printf("Baixo\n");
 }
 
 
