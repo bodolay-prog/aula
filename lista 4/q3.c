eeca8fde180756d17de0c267152e2693a87f2d07
bodolay-prog <caua.bodolay09@aluno.ifce.edu.br>
2025-06-04 19:58:31 +0000

    Final

---
+++ lista 4/q3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+void pedirValor(int *);
+void imprimirMenorValor(int*,int*);
+int main(){
+
+int v0, *p_v0 = &v0,v, *p_v = &v, i = 0;
+
+pedirValor(p_v);
+
+*p_v0=*p_v;
+
+while (i<5){
+
+pedirValor(p_v);
+imprimirMenorValor(p_v0,p_v);
+
+i++;
+}
+
+
+}
+
+void pedirValor(int *pa){ 
+
+    printf("Digite um valor: \n");
+    scanf("%d",pa);
+
+
+
+}
+
+void imprimirMenorValor(int *pv0, int *pv){
+
+
+    if(*pv<*pv0){
+    printf("O menor valor é %d\n",*pv);
+    *pv0 = *pv; 
+}
+else{
+    printf("O menor valor é %d\n",*pv0);
+    
+}
+}
+
+
