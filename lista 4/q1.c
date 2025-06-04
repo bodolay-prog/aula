20ea4b5b3bbe48431342b99b10942d1ede177235
bodolay-prog <caua.bodolay09@aluno.ifce.edu.br>
2025-06-04 19:13:07 +0000

    FInal

--- lista 4/q1.c
+++ lista 4/q1.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 void pedirValor(int *, int *);
-void fazerSoma(int*,int*,int*);
+void fazerSoma_MostrarValor(int*,int*,int*);
 
 int main(){
 
     int a, *p_a = &a, b, *p_b = &b, r, *p_r = &r;
 
     pedirValor(p_a,p_b);
-    fazerSoma(p_a,p_b,p_r);
+    fazerSoma_MostrarValor(p_a,p_b,p_r);
 
 
 }
@@ -24,8 +24,9 @@ void pedirValor(int *pa, int *pb){
 
 }
 
-void fazerSoma(int *pa,int *pb, int *pr){
+void fazerSoma_MostrarValor(int *pa,int *pb, int *pr){
 
     *pr = *pa + *pb;
+    printf("O valor é %d e o endereço é [%p]\n",*pr,pr);
 
 }
