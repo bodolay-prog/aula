2e85b8f02b4e01bcd655a1b99bb58757bc06755a
bodolay-prog <caua.bodolay09@aluno.ifce.edu.br>
2025-06-05 00:24:47 +0000

    Final

--- lista 4/q4.c
+++ lista 4/q4.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #define RANGE 10
+#define START 0
 
 void preencherVetor(int*,int);
 void imprimirVetor(int*,int qtd);
 
 int main(){
 
-srand(time(NULL));
+
 int *p_v=NULL;
 int qtd;
 
 puts("Digite a quantidade de valores do vetor: ");
-    scanf("%d",&qtd);
+scanf("%d",&qtd);
 
-    p_v= malloc(qtd*sizeof(int));
+    p_v = malloc(qtd*sizeof(int));
 
     if(!p_v){
         puts("Memória não dispovível.");
         exit(1);
     }
 
+srand(time(NULL));    
 preencherVetor(p_v,qtd);
 imprimirVetor(p_v,qtd);
 
@@ -29,18 +31,18 @@ imprimirVetor(p_v,qtd);
 
 void preencherVetor(int *pv, int qtd){
 
-    for(int i =0; i<qtd;i++){
+    for(int i = 0; i<qtd ;i++){
 
-        *(pv+i) = rand() % RANGE;
+        *(pv+i) = START + rand() % RANGE;
     }
 
 }
 
 void imprimirVetor(int *pv, int qtd){
 
-    for(int i = 0;i<qtd;i++){
+    for(int i = 0; i<qtd ;i++){
 
-    printf("[%p] - %d\n", pv,*pv);
+    printf("[%p] - %d\n", (pv+i),*(pv+i));
 }
 
 }
