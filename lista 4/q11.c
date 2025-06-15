bb8ec9fab25c1ed5125a5267d2af8ce8b75d6c54
bodolay-prog <caua.bodolay09@aluno.ifce.edu.br>
2025-06-15 17:40:15 +0000

    Refatorar q11.c: ajustar tamanhos de vetor, melhorar funções de preenchimento e impressão

--- lista 4/q11.c
+++ lista 4/q11.c
@@ -3,28 +3,35 @@
 #include <time.h>
 
 #define N 10
-#define TAM 15
 #define START 0
 
-void preencherVetor(int*);
-void imprimirVetor(int*,int);
-void imprimirVetor(int*,int);
-
+void preencherVetor(int*,int);
+void imprimirVetor_reto(int*,int);
+void preencherHisto_matriz(int*,int*,int*,int);
+void imprimirMatriz(int*,int);
 
 int main(){
 
-    int x[TAM];
-    int y[TAM];
+    srand(time(NULL));
 
-    preencherVetor(x);
-    preencherVetor(y);
+    unsigned x[N];
+    unsigned y[N];
+    int m[N*N]={0};
 
+    preencherVetor(x,N);
+    preencherVetor(y,N);
     puts("Vetor X:");
-    imprimirVetor_reto(x,TAM);
+    puts("====================");
+    imprimirVetor_reto(x,N);
+    puts("====================");
     puts("Vetor Y:");
-    imprimirVetor_reto(y,TAM);
+    puts("====================");
+    imprimirVetor_reto(y,N);
+    puts("====================");
+    preencherHisto_matriz(m,x,y,N);
     puts("Matriz Histrograma:");
-
+    imprimirMatriz(m,N);
+    puts("====================");
     
 
 
@@ -32,22 +39,44 @@ int main(){
     return 0;
 }
 
-void preencherVetor(int *v){
-    for(int i = 0; i < N; i++){
-        v[i] = rand() % TAM + START;
+void preencherVetor(int *v,int q){
+    for(int i = 0; i < q; i++){
+        v[i] = START + rand() % q;
     }
-}
-
-void imprimirVetor(int *v, int n){
-    for(int i = 0; i < n; i++){
-        printf("%d ", v[i]);
-    }
-    printf("\n");
 }
 
 void imprimirVetor_reto(int *v, int n){
     for(int i = 0; i < n; i++){
-        printf("%d ", v[i]);
+        printf("%d ", v[i]+1); // mais 1, pois o que o programa entende vem como como o valor - 1 vindo com + 1 fica exatamente como se le
     }
     printf("\n");
+}
+
+void preencherHisto_matriz(int *v,int *v2,int *v3,int qtd){
+
+
+    for(int i = 0;i<qtd;i++){
+         
+
+        (*(v+(*(v2 + i)) * N + (*(v3 + i))))++;
+       
+
+       
+    }
+}
+
+void imprimirMatriz(int *v,int qtd){
+
+    for(int i = 0;i<qtd*qtd;i++){
+
+        if(!(i%N)&&i!=0){
+
+            puts("");
+
+        }
+        printf("%d ",*(v+i));
+        
+
+    }   
+    puts("");
 }
